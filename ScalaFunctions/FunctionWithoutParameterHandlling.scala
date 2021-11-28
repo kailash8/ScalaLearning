94ed2cc6fa9d8e04850cc7284aa7a267a3426039
Kailash Choudhary <48403668+kailash8@users.noreply.github.com>
2021-11-28 20:47:21 +0530

    Update FunctionWithoutParameterHandlling.scala

--- ScalaFunctions/FunctionWithoutParameterHandlling.scala
+++ ScalaFunctions/FunctionWithoutParameterHandlling.scala
@@ -1,12 +1,18 @@
-//@timestamp 20:10 28 Nov 2021
+//@timestamp: 28 Nov 2021 15:16:19 PM
+import java.util.Date
+import java.text.SimpleDateFormat
+
 object MainObject {
     def main(args: Array[String]) = {
         var result1 = FunctionWithoutParameterHandlling(10,2)
         var result2 = FunctionWithoutParameterHandlling(30)
         var result3 = FunctionWithoutParameterHandlling()  //this scenario is being tested
         println(result1 + "\n" + result2 + "\n" + result3)
+        
+        var test = new SimpleDateFormat("dd MMM yyy HH:mm:ss a").format(new Date)
+        println("""//@timestamp: """ +test)
     }
-    def FunctionWithoutParameterHandlling(a: Int = 0, b:Int = 0):Int= { //Parameter with default values
+    def FunctionWithoutParameterHandlling(a: Int = 0, b:Int = 0):Int= {
         a+b
     }
 }
