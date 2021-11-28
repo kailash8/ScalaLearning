18ba629a08ebfe8cd55f395ffbad3eaeac4ed66f
Kailash Choudhary <48403668+kailash8@users.noreply.github.com>
2021-11-28 20:10:44 +0530

    Create FunctionWithoutParameterHandlling.scala

---
+++ ScalaFunctions/FunctionWithoutParameterHandlling.scala
@@ -0,0 +1,12 @@
+//@timestamp 20:10 28 Nov 2021
+object MainObject {
+    def main(args: Array[String]) = {
+        var result1 = FunctionWithoutParameterHandlling(10,2)
+        var result2 = FunctionWithoutParameterHandlling(30)
+        var result3 = FunctionWithoutParameterHandlling()  //this scenario is being tested
+        println(result1 + "\n" + result2 + "\n" + result3)
+    }
+    def FunctionWithoutParameterHandlling(a: Int = 0, b:Int = 0):Int= {
+        a+b
+    }
+}
